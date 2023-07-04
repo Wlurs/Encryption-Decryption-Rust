def45af011b61c673f2a116d4ac7bb76cb945403
Wlurs <lqdprigat@gmail.com>
2023-07-04 21:11:51 +0300

    Use Crates for opts

--- encryptDecrypt/src/main.rs
+++ encryptDecrypt/src/main.rs
@@ -1,14 +1,26 @@
-use std::env;
+// use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+// use std::process::Output;
+// use std::path::PathBuf;
 
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    #[arg(short, long)]
+    path: String,
+
+    #[arg(short, long)]
+    shift: u8,
+}
 
 fn main() {
-    println!("Hello, world!");
+    
+    let cli = Cli::parse();
+    let data: String = read_file(&cli.path as &str);
+    let shift: u8 = cli.shift;
 
-    let arg: Vec<String> = env::args().collect();
-    let data: String = read_file(&arg[1]);
-    let shift: u8 = arg[2].parse().expect("Invalid shift value");
     dbg!(&caesar_encrypt(&data, shift));
 }
 
@@ -17,7 +29,6 @@ fn read_file(path: &str) -> String
     let mut file = File::open(path).expect("File not found");
     let mut data = String::new();
     file.read_to_string(&mut data).expect("Error reading file");
-    // println!("File contents:\n{}", data);
     return data;
 }
 
@@ -25,10 +36,8 @@ fn caesar_encrypt(data: &String, shift: u8) -> String
 {
     let mut encrypted = String::new();
     for c in data.chars(){
-        // let shifted_char = ((c as u8 + shift) as char).to_string();
         encrypted.push( (c  as u8 + shift as u8) as char);
     }
-
     return encrypted;
 }
 
